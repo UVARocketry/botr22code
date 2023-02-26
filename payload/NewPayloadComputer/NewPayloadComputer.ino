4ef02d8875b4eb77433068f9253f73dea9194732
Edison Wong <edisonwong2004@gmail.com>
2023-02-26 00:23:00 -0500

    Updated receive test code and newPayloadComputer code

--- payload/NewPayloadComputer/NewPayloadComputer.ino
+++ payload/NewPayloadComputer/NewPayloadComputer.ino
@@ -1,9 +1,16 @@
 #include <Adafruit_GPS.h>
+//#include <SPI.h>
+//#include <SD.h>
 
-//may be defined to incorrect serial ports rn, will fix
+int commaCounter = 0;
+int currentIndexInBuffer = 0;
+char remoteDataBuffer [40];
+char buffer [120];
+
+//pay attention to correct ports
 #define XbeeR Serial1
-#define XbeeT Serial3
 #define GPSSerial Serial2
+#define XbeeT Serial3
 
 // Connect to the GPS on the hardware port
 Adafruit_GPS GPS(&GPSSerial);
@@ -23,6 +30,8 @@ void setup() {
   GPS.begin(9600);
   //Serial.println("GPS Serial begun!");
 
+  //change to the correct cs pin
+  //SD.begin(cspin);
   
   // turn on RMC (recommended minimum) and GGA (fix data) including altitude
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
@@ -38,54 +47,95 @@ void setup() {
 
 void loop() {
 
-  // read data from the GPS in the 'main loop'
-  char c = GPS.read();
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
-  }
-  //uncommented gps data that we probably don't need, may readd/delete late
-  // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) {
-    timer = millis(); // reset the timer
-      if(XbeeR.available()){
-        if(XbeeT.available()){
-          //Transmits Remote Sensor Data
-          XbeeT.print(XbeeR.read());
-        
-          //Transmits GPS Data
-          XbeeT.println("Time: ");
-          if (GPS.hour < 10) { XbeeT.print('0'); }
-          XbeeT.print(GPS.hour, DEC); XbeeT.print(':');
-          if (GPS.minute < 10) { XbeeT.print('0'); }
-          XbeeT.print(GPS.minute, DEC); XbeeT.print(':');
-          if (GPS.seconds < 10) { XbeeT.print('0'); }
-          XbeeT.print(GPS.seconds, DEC); XbeeT.print('.');
-          //XbeeT.print(GPS.milliseconds);
-          //XbeeT.print("Date: ");
-          //XbeeT.print(GPS.day, DEC); XbeeT.print('/');
-          //XbeeT.print(GPS.month, DEC); XbeeT.print("/20");
-          //XbeeT.println(GPS.year, DEC);
-          //XbeeT.print("Fix: "); XbeeT.print((int)GPS.fix);
-          //XbeeT.print(" quality: "); XbeeT.println((int)GPS.fixquality);
-          if (GPS.fix) {
-            XbeeT.print("Location: ");
-            XbeeT.print(GPS.latitude, 4); XbeeT.print(GPS.lat);
-            XbeeT.print(", ");
-            XbeeT.print(GPS.longitude, 4); XbeeT.print(GPS.lon); //don't know why there's two prints
-            XbeeT.print("Speed (knots): "); XbeeT.println(GPS.speed); //don't know why there's two prints
-            XbeeT.print("Angle: "); XbeeT.println(GPS.angle);
-            XbeeT.print("Altitude: "); XbeeT.println(GPS.altitude);
-            XbeeT.print("Satellites: "); XbeeT.println((int)GPS.satellites);
-            //XbeeT.print("Antenna status: "); XbeeT.println((int)GPS.antenna);
-          }
+  commaCounter = 0;
+  currentIndexInBuffer = 0;
+  remoteDataBuffer[0] = { 0 };
+  buffer[0] = { 0 };
+
+  remoteDataBuffer [0] = XbeeR.read();
+    //not sure if '1' or 1
+    if(remoteDataBuffer[0] == '1' || '2'){
+      while(commaCounter < 5){
+        currentIndexInBuffer++;
+        remoteDataBuffer [currentIndexInBuffer] = XbeeR.read();
+        if(remoteDataBuffer [currentIndexInBuffer] == ','){
+          commaCounter++;
         }
       }
-    }  
-  }
+      //gets three chars more after the forth comma
+      for(int i = 0; i < 3; i++){
+        currentIndexInBuffer++;
+        remoteDataBuffer [currentIndexInBuffer] == XbeeR.read();
+      }
+    }
+  //parses all the commas
+  char *sensNum = strtok(remoteDataBuffer, ",");
+  char *pressure = strtok(NULL, ",");
+  char *temp = strtok(NULL, ",");
+  char *humidity = strtok(NULL, ",");
+  char *solarVolt = strtok(NULL, ",");
+  //is timer necessary to prevent the processor from overloading?
+  char gpsHour[] = GPS.hour;
+  char gpsMin[] = GPS.minute;
+  char gpsSec[] = GPS.seconds;
+  char gpsMSec [] = GPS.milliseconds;
+  char gpsLat [] = GPS.latitude;
+  char gpsLong [] = GPS.longitude;
+  char gpsSpeed [] = GPS.speed;
+  char gpsAngle [] = GPS.angle;
+  char gpsAltitude [] = GPS.altitude;
+  char gpsSatellites [] = GPS.satellites;
+
+  //concatenates the remote sensor and gps data
+  char buffer [150];
+  sprintf(buffer, "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s", 
+  sensNum, pressure, temp, humidity, solarVolt, gpsHour, gpsMin, gpsSec, gpsMSec, gpsLat, gpsLong, gpsSpeed, gpsAngle, gpsAltitude, gpsSatellites);
+  XbeeT.println(buffer);
+
+  //data logging
+  //payloadDataFile = SD.open("payloadData.txt", FILE_WRITE);
+  //payloadDataFile.write(buffer);
+  //SD.close("payloadData.txt");
+
+
+  //Will delete later: keep for reference
+  //uncommented gps data that we probably don't need, may read/delete late
+  // approximately every 2 seconds or so, print out the current stats
+  // if (millis() - timer > 2000) {
+  //   timer = millis(); // reset the timer
+  //     if(XbeeR.available()){
+  //       if(XbeeT.available()){
+  //         //Transmits Remote Sensor Data
+  //         XbeeT.print(XbeeR.read());
+        
+  //         //Transmits GPS Data
+  //         XbeeT.println("Time: ");
+  //         if (GPS.hour < 10) { XbeeT.print('0'); }
+  //         XbeeT.print(GPS.hour, DEC); XbeeT.print(':');
+  //         if (GPS.minute < 10) { XbeeT.print('0'); }
+  //         XbeeT.print(GPS.minute, DEC); XbeeT.print(':');
+  //         if (GPS.seconds < 10) { XbeeT.print('0'); }
+  //         XbeeT.print(GPS.seconds, DEC); XbeeT.print('.');
+  //         //XbeeT.print(GPS.milliseconds);
+  //         //XbeeT.print("Date: ");
+  //         //XbeeT.print(GPS.day, DEC); XbeeT.print('/');
+  //         //XbeeT.print(GPS.month, DEC); XbeeT.print("/20");
+  //         //XbeeT.println(GPS.year, DEC);
+  //         //XbeeT.print("Fix: "); XbeeT.print((int)GPS.fix);
+  //         //XbeeT.print(" quality: "); XbeeT.println((int)GPS.fixquality);
+  //         if (GPS.fix) {
+  //           XbeeT.print("Location: ");
+  //           XbeeT.print(GPS.latitude, 4); XbeeT.print(GPS.lat);
+  //           XbeeT.print(", ");
+  //           XbeeT.print(GPS.longitude, 4); XbeeT.print(GPS.lon); //don't know why there's two prints
+  //           XbeeT.print("Speed (knots): "); XbeeT.println(GPS.speed); //don't know why there's two prints
+  //           XbeeT.print("Angle: "); XbeeT.println(GPS.angle);
+  //           XbeeT.print("Altitude: "); XbeeT.println(GPS.altitude);
+  //           XbeeT.print("Satellites: "); XbeeT.println((int)GPS.satellites);
+  //           //XbeeT.print("Antenna status: "); XbeeT.println((int)GPS.antenna);
+  //         }
+  //       }
+  //     }
+  //   }  
+  //}
 }
--- payload/Receive, Strtok, and sprintf test/Receive, Strtok, and sprintf test.ino
+++ payload/Receive, Strtok, and sprintf test/Receive, Strtok, and sprintf test.ino
@@ -1,24 +1,90 @@
+int commaCounter = 0;
+int currentIndexInBuffer = 0;
+char remoteDataBuffer [40];
+char buffer [120];
+//String remoteData = "";
+
+
 void setup() {
   Serial.begin(9600);
-  //Serial1.begin(9600);
-
+  Serial1.begin(9600);
+  
 }
 
 void loop() {
-  //char remoteDataBuffer[] = Serial1.read();
+  //resets all the counters
+  commaCounter = 0;
+  currentIndexInBuffer = 0;
+  remoteDataBuffer[0] = { 0 };
+  buffer[0] = { 0 };
 
-  char remoteDataBuffer [] = "1,100.123,21.0,31,6.7";
+  remoteDataBuffer [0] = Serial1.read();
+    //not sure if '1' or 1
+    if(remoteDataBuffer[0] == '1' || '2'){
+      while(commaCounter < 5){
+        currentIndexInBuffer++;
+        remoteDataBuffer [currentIndexInBuffer] = Serial1.read();
+        if(remoteDataBuffer [currentIndexInBuffer] == ','){
+          commaCounter++;
+        }
+      }
+      //gets three chars more after the forth comma
+      for(int i = 0; i < 3; i++){
+        currentIndexInBuffer++;
+        remoteDataBuffer [currentIndexInBuffer] == Serial1.read();
+      }
+    }
+  //parses all the commas
   char *token1 = strtok(remoteDataBuffer, ",");
   char *token2 = strtok(NULL, ",");
   char *token3 = strtok(NULL, ",");
   char *token4 = strtok(NULL, ",");
   char *token5 = strtok(NULL, ",");
-  char longitude = 400;
-  char latitude = 500;
-  char speed = 100;
+  char longitude[] = "400";
+  char latitude[] = "500";
+  char speed[] = "100";
 
+  //concatenates the remote sensor and gps data
   char buffer [120];
-  sprintf(buffer, "Sensor: %c, Pressure: %d, Temp: %d, Humidity: %d, SV: %d, Long: %d, Lat: %d, Speed: %d", 
-  token1, token2, token 3, token4, token5, longitude, latitude, speed);
+  sprintf(buffer, "Sensor: %s, Pressure: %s, Temp: %s, Humidity: %s, SV: %s, Long: %s, Lat: %s, Speed: %s", 
+  token1, token2, token3, token4, token5, longitude, latitude, speed);
   Serial.println(buffer);
+
+  // if (counter == 4) {
+  //   i++;
+  //   remoteData += receive;
+
+  //   if (i == 3) {
+  //     counter = 0;
+  //     i = 0;
+  //     char remoteDataBuffer[40];
+  //     remoteData.toCharArray(remoteDataBuffer, remoteData.length());
+  //     Serial.println(remoteDataBuffer);
+  //     remoteData = "";
+  //   }
+
+  // } else {
+
+  //   if (receive == ',') {
+  //     counter++;
+  //   }
+
+  //   remoteData += receive;
+  // }
+
+  // char remoteDataBuffer [] = "1,100.123,21.0,31,6.7";
+  // char *token1 = strtok(remoteDataBuffer, ",");
+  // char *token2 = strtok(NULL, ",");
+  // char *token3 = strtok(NULL, ",");
+  // char *token4 = strtok(NULL, ",");
+  // char *token5 = strtok(NULL, ",");
+  // char longitude[] = "400";
+  // char latitude[] = "500";
+  // char speed[] = "100";
+
+  // char buffer [120];
+  // sprintf(buffer, "Sensor: %s, Pressure: %s, Temp: %s, Humidity: %s, SV: %s, Long: %s, Lat: %s, Speed: %s", 
+  // token1, token2, token3, token4, token5, longitude, latitude, speed);
+  // Serial.println(buffer);
 }
+
