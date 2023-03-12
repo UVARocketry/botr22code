5077f9304d66b8706b998c913494833edf51837f
Edison Wong <edisonwong2004@gmail.com>
2023-03-12 16:28:14 -0400

    Updated Parsing for two remote sensors and using global instead of local variables

--- GroundStation/xbee receive test.py
+++ GroundStation/xbee receive test.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-#
+#Global Variables For Parsing
 sensNum = ""
 gpsHour = ""
 gpsMin = ""
@@ -13,10 +13,6 @@ gpsSpeed = ""
 gpsAngle = ""
 gpsAltitude = ""
 gpsSatellites = ""
-temp = ""
-pressure =  ""
-humidity = ""
-solarVolt = ""
 tempOne = ""
 pressureOne = ""
 humidityOne = ""
@@ -25,9 +21,6 @@ tempTwo = ""
 pressureTwo = ""
 humidityTwo = ""
 solarVoltTwo = ""
-
-
-
 
 ser = serial.Serial(port = "COM3", baudrate = 9600)
 
@@ -48,34 +41,32 @@ while True:
     gpsAngle = tempBuffer[8]
     gpsAltitude = tempBuffer[9]
     gpsSatellites = tempBuffer[10]
-    temp = tempBuffer[11]
-    pressure = tempBuffer [12]
-    humidity = tempBuffer[13]
-    solarVolt = tempBuffer[14]
+
+    if sensNum == "1":
+        #print("Sensor One Data Received")
+        tempOne = tempBuffer[11]
+        pressureOne = tempBuffer [12]
+        humidityOne = tempBuffer[13]
+        solarVoltOne = tempBuffer[14]
+    if sensNum == "2":
+        #print("Sensor Two Data Received")
+        tempTwo = tempBuffer[11]
+        pressureTwo = tempBuffer [12]
+        humidityTwo = tempBuffer[13]
+        solarVoltTwo = tempBuffer[14]
+
     rs1_data_counter = tempBuffer[15]
     rs2_data_counter = tempBuffer[16]
     gps_data_counter = tempBuffer[17]
 
-    if sensNum == "1":
-        print("Sensor One Data Received")
-        tempOne = temp;
-        pressureOne = pressure;
-        humidityOne = humidity;
-        solarVoltOne = solarVolt;
-    if sensNum == "2":
-        print("Sensor Two Data Received")
-        tempTwo = temp;
-        pressureTwo = pressure;
-        humidityTwo = humidity;
-        solarVoltTwo = solarVolt;
-    
-    print("sensNum: %s, gpsHour: %s, gpsMin: %s, gpsSec: %s, gpsMSec: %s, gpsLong: %s, gpsLat: %s, gpsSpeed: %s, gpsAngle: %s, gpsAltitude: %s, gpsSatellites: %s, temp: %s, pressure: %s, humidity: %s, solarVolt: %s, rs1_data_counter: %s, rs2_data_counter: %s, gps_data_counter: %s" 
-    % (sensNum, gpsHour, gpsMin, gpsSec, gpsMSec, gpsLong, gpsLat, gpsSpeed, gpsAngle, gpsAltitude, gpsSatellites, temp, pressure, humidity, solarVolt, rs1_data_counter, rs2_data_counter, gps_data_counter))
+    # if sensNum == "1":
+    #     print("sensNum: %s, gpsHour: %s, gpsMin: %s, gpsSec: %s, gpsMSec: %s, gpsLong: %s, gpsLat: %s, gpsSpeed: %s, gpsAngle: %s, gpsAltitude: %s, gpsSatellites: %s, temp: %s, pressure: %s, humidity: %s, solarVolt: %s, rs1_data_counter: %s, rs2_data_counter: %s, gps_data_counter: %s" 
+    #     % (sensNum, gpsHour, gpsMin, gpsSec, gpsMSec, gpsLong, gpsLat, gpsSpeed, gpsAngle, gpsAltitude, gpsSatellites, tempOne, pressureOne, humidityOne, solarVoltOne, rs1_data_counter, rs2_data_counter, gps_data_counter))
+
+    # if sensNum == "2":
+    #     print("sensNum: %s, gpsHour: %s, gpsMin: %s, gpsSec: %s, gpsMSec: %s, gpsLong: %s, gpsLat: %s, gpsSpeed: %s, gpsAngle: %s, gpsAltitude: %s, gpsSatellites: %s, temp: %s, pressure: %s, humidity: %s, solarVolt: %s, rs1_data_counter: %s, rs2_data_counter: %s, gps_data_counter: %s" 
+    #     % (sensNum, gpsHour, gpsMin, gpsSec, gpsMSec, gpsLong, gpsLat, gpsSpeed, gpsAngle, gpsAltitude, gpsSatellites, tempTwo, pressureTwo, humidityTwo, solarVoltTwo, rs1_data_counter, rs2_data_counter, gps_data_counter))
 
     time.sleep(1)
 
-    if(keyboard.is_pressed('q')):
-        print("User needs to quit")
-        break
-
-ser.close()
+#ser.close()
