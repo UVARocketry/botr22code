eed15bb0fdddada1a9c845fbee8f2e46bd61b26b
Edison Wong <edisonwong2004@gmail.com>
2023-03-25 23:07:39 -0400

    Added the gps graph

---
+++ GroundStation/GPSSensor.py
@@ -0,0 +1,54 @@
+import random
+from itertools import count
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib import style
+import XbeeReceive
+import main
+import datetime
+
+class GPS:
+    def __init__(self):
+        self.xbee = main.returnXbee()
+        
+        self.plt3 = plt
+
+        style.use('fivethirtyeight')
+        
+        # Declares gps graph
+        # Don't know if subplots are necessary for the gps graph
+        self.fig3, self.axl3 = self.plt3.subplots()
+        self.axl3.set_title("GPS Data")
+        self.axl3.set_xlabel("Time")
+        self.axl3.grid(True)
+
+        self.gpsTimeList = []
+
+        # data from gps
+        self.indexThree = count()
+        self.g3Data = [[], []]
+        self.g3DataLines = []
+        for j in range(2):
+            self.g3DataLines.append(self.axl3.plot([],[])[0])
+
+    def animate(self, i, gps, data, lines, ax, index):
+        gps.append(next(index))
+        
+        data[0].append(int(float(self.xbee.returnGPSData[0])))
+        data[1].append(int(float(self.xbee.returnGPSData[1])))
+        
+        for j in range(2):
+            lines[j].set_data(gps, data[j])
+            ax.relim()
+            ax.autoscale_view()
+
+    def animation(self):
+        self.state = int(float(self.xbee.returnState()))
+        
+        if ((self.state == 1) | (self.state == 2)):
+            self.aniThree = FuncAnimation(self.fig3, self.animate, frames=None, cache_frame_data=False, fargs=(self.gpsTimeList, self.g3Data, self.g3DataLines, self.axl3, self.indexThree))
+    
+    def returnGraphG3(self):
+        return self.fig3
